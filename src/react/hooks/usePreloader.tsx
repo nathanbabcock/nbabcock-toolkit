c64ab26d0e3f3432398a99be5b64962975408ab6
nbabcock <nathan.r.babcock@gmail.com>
2022-02-28 12:08:46 -0600

    feat(hooks): Handle maxWidth and maxHeight in usePreloader

--- src/react/hooks/usePreloader.tsx
+++ src/react/hooks/usePreloader.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 
+export type PreloadOptions = {
+  maxWidth?: number;
+  maxHeight?: number;
+  width?: number;
+  height?: number;
+};
+
 /**
  * Preloads and resizes an image.
  *
@@ -13,8 +20,7 @@ import { useEffect, useRef, useState } from "react";
  */
 export function preload(
   url: string,
-  width?: number,
-  height?: number
+  options?: PreloadOptions
 ): Promise<ImageData> {
   return new Promise((resolve, reject) => {
     const image = document.createElement("img");
@@ -23,8 +29,20 @@ export function preload(
     image.src = url;
     image.onerror = reject;
     image.onload = () => {
-      canvas.width = width ?? image.width;
-      canvas.height = height ?? image.height;
+      let width = options?.width ?? image.width;
+      let height = options?.height ?? image.height;
+      if (options?.maxWidth && width > options.maxWidth) {
+        const scale = options.maxWidth / width;
+        width = options.maxWidth;
+        height *= scale;
+      }
+      if (options?.maxHeight && height > options.maxHeight) {
+        const scale = options.maxHeight / height;
+        height = options.maxHeight;
+        width *= scale;
+      }
+      canvas.width = width;
+      canvas.height = height;
       const ctx = canvas.getContext("2d");
       if (!ctx) return reject("Failed to get canvas context");
       ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
@@ -51,8 +69,7 @@ export type UsePreloadHook = {
   /** Preloads an image, optionally resizes it, stores status and loaded ImageData in state variables */
   preload: (
     url: string,
-    width?: number,
-    height?: number
+    options?: PreloadOptions
   ) => Promise<ImageData | undefined>;
 
   /**
@@ -63,8 +80,8 @@ export type UsePreloadHook = {
 };
 
 /**
- * Wraps image preloading functionality in a React hook,
- * handling state for loading status, errors, and final image data.
+ * Wraps the image preloading functionality in a React hook,
+ * handling state for loading, errors, and image data.
  */
 export default function usePreloader(): UsePreloadHook {
   const [imageData, setImageData] = useState<UsePreloadHook["imageData"]>({});
@@ -72,12 +89,12 @@ export default function usePreloader(): UsePreloadHook {
   const done = Object.values(status).every(
     (status) => status === PreloadStatus.LOADED
   );
-  const _preload = async (url: string, width?: number, height?: number) => {
+  const _preload = async (url: string, options?: PreloadOptions) => {
     if ([PreloadStatus.LOADING, PreloadStatus.LOADED].includes(status[url]))
       return;
     setStatus((prev) => ({ ...prev, [url]: PreloadStatus.LOADING }));
     try {
-      const imageData = await preload(url, width, height);
+      const imageData = await preload(url, options);
       setImageData((prev) => ({ ...prev, [url]: imageData }));
       return imageData;
     } catch (e) {
@@ -89,7 +106,7 @@ export default function usePreloader(): UsePreloadHook {
   return { preload: _preload, imageData, status, done } as UsePreloadHook;
 }
 
-export type PreloadedImageProps = {
+export type PreloadImageProps = {
   imageData?: ImageData;
   [key: string]: any;
 };
@@ -98,15 +115,17 @@ export type PreloadedImageProps = {
  * A simple outlet for rendering ImageData
  * (intended to be used with {@link usePreload})
  */
-export function PreloadedImage({ imageData, ...props }: PreloadedImageProps) {
+export function PreloadedImage({ imageData, ...props }: PreloadImageProps) {
   const canvas = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (!canvas.current || !imageData) return;
+    if (!canvas.current) return;
     const ctx = canvas.current.getContext("2d");
     if (!ctx) return;
     ctx.imageSmoothingEnabled = true;
     ctx.imageSmoothingQuality = "high";
+    ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+    if (!imageData) return;
     ctx.putImageData(imageData, 0, 0);
   }, [imageData]);
   return (
