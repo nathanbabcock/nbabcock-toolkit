8d79168966edda085292bc46c1611959366b0f68
nbabcock <nathan.r.babcock@gmail.com>
2022-09-15 13:38:26 -0500

    docs(util): add jsdoc to randInRange

--- src/util/randInRange.ts
+++ src/util/randInRange.ts
@@ -1,3 +1,4 @@
-export default function randInRange(min: number, max: number) { // min and max included 
+/** @returns a random integer between min and max (both inclusive) */
+export default function randInRange(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
