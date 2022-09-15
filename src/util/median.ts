1fd76cd5469956b138fc692d688b9dfabf716be0
nbabcock <nathan.r.babcock@gmail.com>
2022-09-15 13:32:18 -0500

    feat(util): add median

---
+++ src/util/median.ts
@@ -0,0 +1,7 @@
+export function median(values: number[]) {
+  if (values.length === 0) return NaN
+  values.sort((a, b) => a - b)
+  const half = Math.floor(values.length / 2)
+  if (values.length % 2) return values[half]
+  return (values[half - 1] + values[half]) / 2.0
+}
