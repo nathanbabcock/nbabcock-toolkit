c711d3d2731ed8d00d24bbb2ec52fad5c3f313b9
nbabcock <nathan.r.babcock@gmail.com>
2022-10-01 15:43:14 -0500

    refactor(cli): check for upstream before deleting repo in gh alias

--- src/cli/github-cli.sh
+++ src/cli/github-cli.sh
@@ -8,4 +8,4 @@ gh alias set fork '!gh repo fork $1 --clone "$(basename $1)" && code "$(basename
 gh alias set pullrequest '!git push -u && gh pr create -w --repo "$(git remote get-url upstream)"'
 
 # Delete remote & local
-gh alias set rm '!gh repo delete $1 --confirm && rm -rf $1'
+gh alias set rm '!git -C $1 remote get-url upstream && gh repo delete $1 --confirm && rm -rf $1'
