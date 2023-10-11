edc8b2c818676ba6738545aabd26763f74e1a0b2
nbabcock <nathan.r.babcock@gmail.com>
2023-10-11 15:18:18 -0500

    refactor: update clamp api

--- src/util/clamp.ts
+++ src/util/clamp.ts
@@ -1,3 +1,3 @@
-const clamp = (num: number, [min, max]: [number, number]) =>
+/** Constrain `num` in a range between `min` and `max` (inclusive) */
+export const clamp = (num: number, min: number, max: number) =>
   Math.min(Math.max(num, min), max)
-export default clamp
