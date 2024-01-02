856f002efeff8c0c6a48f0bb345dd23f0d8c8289
nbabcock <nathan.r.babcock@gmail.com>
2024-01-02 13:53:27 -0600

    docs(utils): add jsdoc for quantity

--- src/util/quantity.ts
+++ src/util/quantity.ts
@@ -1,3 +1,10 @@
+/**
+ * ```
+ * quantity(0, "apple") === "0 apples"
+ * quantity(1, "orange") === "1 orange"
+ * quantity(2, "banana") === "2 bananas"
+ * ```
+ */
 export function quantity(
   count: number,
   singular: string,
