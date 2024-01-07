c0a99058737dc3a3d9d5256e6eec2b006c33204b
nbabcock <nathan.r.babcock@gmail.com>
2024-01-07 14:54:54 -0600

    refactor(hooks): rewrite reverse mapped type without as

--- src/react/hooks/useEventListener.ts
+++ src/react/hooks/useEventListener.ts
@@ -1,23 +1,41 @@
 import { type RefObject, useEffect } from 'react'
 
-// "Reverse mapped type" approach from this video: https://portal.gitnation.org/contents/infer-multiple-things-at-once-with-reverse-mapped-types
-type PossibleEventType<K> = K extends `on${infer Type}` ? Type : never
-type GetEvent<Name> = Name extends keyof HTMLElementEventMap
-  ? HTMLElementEventMap[Name]
-  : Event
-
+/** e.g. `useEventListener(document, "click", () => {})` */
 export function useEventListener<
-  T extends EventTarget,
-  E extends PossibleEventType<keyof T>
+  TTarget extends EventTarget = EventTarget,
+  TType extends string = string
 >(
-  targetOrRef: T | RefObject<T>,
-  eventName: E,
-  handler: (event: GetEvent<E>) => void
+  targetOrRef: TTarget | RefObject<TTarget>,
+  type: TType,
+  listener: Listener<TTarget, TType>,
+  options?: boolean | AddEventListenerOptions
 ) {
   return useEffect(() => {
-    const element = 'current' in targetOrRef ? targetOrRef.current : targetOrRef
-    element?.addEventListener(eventName, handler as EventListener)
-    return () =>
-      element?.removeEventListener(eventName, handler as EventListener)
-  }, [targetOrRef, eventName, handler])
+    const target = 'current' in targetOrRef ? targetOrRef.current : targetOrRef
+    target?.addEventListener(type, listener, options)
+    return () => target?.removeEventListener(type, listener, options)
+  }, [targetOrRef, type, listener, options])
 }
+
+// Provide event typesafety via reverse mapped types:
+// - https://github.com/alexreardon/bind-event-listener/blob/master/src/types.ts
+// - https://portal.gitnation.org/contents/infer-multiple-things-at-once-with-reverse-mapped-types
+type InferEvent<
+  TTarget,
+  TType extends string
+> = `on${TType}` extends keyof TTarget
+  ? Parameters<Extract<TTarget[`on${TType}`], UnknownFunction>>[0]
+  : Event
+
+// For listener objects, the handleEvent function has the object as the `this` binding
+type ListenerObject<TEvent extends Event> = {
+  handleEvent(this: ListenerObject<TEvent>, event: TEvent): void
+}
+
+// event listeners can be an object or a function
+type Listener<TTarget extends EventTarget, TType extends string> =
+  | ListenerObject<InferEvent<TTarget, TType>>
+  | { (this: TTarget, ev: InferEvent<TTarget, TType>): void }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UnknownFunction = (...args: any[]) => any
